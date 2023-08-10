bbe35299152b8403a158d6b6c67c275e32f3d200
Uzair Saleem <uzairsaleemdev@gmail.com>
2023-08-10 18:21:00 +0500

    The pages completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import styles from "./App.module.css"
 import {Route,Routes,BrowserRouter} from "react-router-dom"
 import Blog from "./pages/Blog/Blog";
 import BlogDetail from "./pages/BlogDetail/BlogDetail";
+import Create from "./pages/Create/Create";
+import Update from "./pages/Update/Update";
 function App() {
   return (
     <div className={styles.app}>
@@ -16,6 +18,17 @@ function App() {
       <Route element={
         <Blog/>
       } exact path="/" />
+
+
+
+      <Route element={
+        <Update/>
+      } exact path="/update" />
+ 
+ 
+      <Route element={
+        <Create/>
+      } exact path="/create" />
 
 
 
--- src/pages/Create/Create.jsx
+++ src/pages/Create/Create.jsx
@@ -7,6 +7,20 @@ export default function Create (){
     return(
         <div className={styles.main}>
 
+            <h1 className={styles.mainHeading}>
+                CREATE BLOG POST
+            </h1>
+            <div className={styles.form}>
+                
+                <input className={styles.title} type="text" />
+                <input className={styles.author} type="text" />
+                <textarea className={styles.descrition} name="" id="" cols="30" rows="10"></textarea>
+                <textarea className={styles.content} name="" id="" cols="30" rows="10"></textarea>
+                <input type="file" name="" id="" />
+            </div>
+
+
+
         </div>
     )
 }
