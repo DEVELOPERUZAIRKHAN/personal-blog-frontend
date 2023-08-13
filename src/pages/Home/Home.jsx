bf91152eb32e77b586932b7696e56832da26cc34
Uzair Saleem <uzairsaleemdev@gmail.com>
2023-08-13 13:11:52 +0500

    second section completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -31,7 +31,7 @@ return (
             <header style={{background:`url(${blogs[0].photo})`,backgroundSize:'cover',backgroundPosition:'center',backgroundRepeat:'no-repeat'}} className={styles.header}>
 
             <div className={styles.white}></div>
-        
+        <div className={styles.container}>
             <div className={styles.heroBox}>
                 <h1 className={styles.mainHeading}>Time to get your house clean and in order</h1>
                 <p className={styles.mainPara}>
@@ -39,19 +39,44 @@ return (
                 </p>
                 <button className={styles.mainButton}>Read Article</button>
             </div>
+        </div>
             </header>
 
             <section className={styles.firstSection}>
                 <div className={styles.leftFirst}>
                     <div className={styles.firstItem}>
-
+                        <img className={styles.leftPhoto} src={`${blogs[0].photo}`} alt="" />
+                        <h3 className={styles.leftHeading}>Time to get your house clean and in order</h3>
+                        <p className={styles.leftPara}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure odio cumque adipisci perspiciatis molestiae fuga corporis aliquam.</p>
+                        <button className={styles.leftButton}>Read Article</button>
                     </div>
                     <div className={styles.firstItem}>
-                        <img src={`${blogs[0].photo}`} alt="" />
-                        <h3 className={styles.itemHeading}></h3>
+                        <img className={styles.leftPhoto} src={`${blogs[0].photo}`} alt="" />
+                        <h3 className={styles.leftHeading}>Time to get your house clean and in order</h3>
+                        <p className={styles.leftPara}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure odio cumque adipisci perspiciatis molestiae fuga corporis aliquam.</p>
+                        <button className={styles.leftButton}>Read Article</button>
                     </div>
                 </div>
-                <div className={styles.rightFirst}></div>
+                <div className={styles.rightFirst}>
+                    <div className={styles.rightItem}>
+                        <h3 className={styles.rightHeading}>Time to get your house clean and in order</h3>
+                        <p className={styles.rightPara}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure odio cumque adipisci perspiciatis molestiae fuga corporis aliquam.</p>
+                        <button className={styles.rightButton}>Read this article</button>
+
+                    </div>
+                    <div className={styles.rightItem}>
+                        <h3 className={styles.rightHeading}>Time to get your house clean and in order</h3>
+                        <p className={styles.rightPara}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure odio cumque adipisci perspiciatis molestiae fuga corporis aliquam.</p>
+                        <button className={styles.rightButton}>Read this article</button>
+ 
+                    </div>
+                    <div className={styles.rightItem}>
+                        <h3 className={styles.rightHeading}>Time to get your house clean and in order</h3>
+                        <p className={styles.rightPara}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure odio cumque adipisci perspiciatis molestiae fuga corporis aliquam.</p>
+                        <button className={styles.rightButton}>Read this article</button>
+
+                    </div>
+                </div>
             </section>
         </div>
     )
